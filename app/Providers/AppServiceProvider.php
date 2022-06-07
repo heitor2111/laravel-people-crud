b95f61cca8503b74fc85f9a576c37eb78b987323
Heitor Henrique <heitor2111@gmail.com>
2022-06-06 23:51:32 -0300

    Changes on App Service Provider
    Added "Sanctum:ignoreMigrations()" command to ignore "personal_access_tokens" migration.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Laravel\Sanctum\Sanctum;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        /**
+         * Ignore "create_personal_access_tokens_table" migration.
+         *
+         * @return void
+         */
+        Sanctum::ignoreMigrations();
     }
 
     /**
