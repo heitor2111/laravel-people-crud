ecf23587fcef8b7d04efb04a491d147ed2d8254a
Heitor Henrique <heitor2111@gmail.com>
2022-06-07 23:56:12 -0300

    Created all database seeds

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +13,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $professions = \App\Models\Profession::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\Person::factory(100)
+            ->hasContacts(3)
+            ->hasUsers(1)
+            ->create()
+            ->each(function ($person) use ($professions) {
+                $person->professions()->attach($professions->random());
+            });
     }
 }
