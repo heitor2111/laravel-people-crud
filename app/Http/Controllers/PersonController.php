729c924a1b121b28b7ece8c3bf0cae45562fbed5
Heitor Henrique <heitor2111@gmail.com>
2022-06-08 22:19:42 -0300

    Update on Person Controller
    Finished view, show and delete methods.

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Person;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class PersonController extends Controller
 {
@@ -14,7 +15,19 @@ class PersonController extends Controller
      */
     public function index()
     {
-        return view('person.index');
+        try {
+            // Select the first 20 people sorted by creation date.
+            $people = Person::orderByDesc('created_at')
+                ->paginate(20);
+
+            // Call the Person/Index view, sending the people data.
+            return view(
+                'person.index',
+                ['people' => $people]
+            );
+        } catch (\Throwable $th) {
+            throw $th;
+        }
     }
 
     /**
@@ -44,9 +57,23 @@ class PersonController extends Controller
      * @param  \App\Models\Person  $person
      * @return \Illuminate\Http\Response
      */
-    public function show(Person $person)
+    public function show(Request $request)
     {
-        return view('person.show');
+        try {
+            // Select the person with id is equal to received id
+            // on route, along with all your relationships.
+            $person = Person::with(['contacts', 'professions', 'users'])
+                ->where('id', $request->id)
+                ->first();
+
+            // Call the Person/Show view, sending the person data.
+            return view(
+                'person.show',
+                ['person' => $person]
+            );
+        } catch (\Throwable $th) {
+            throw $th;
+        }
     }
 
     /**
@@ -80,6 +107,42 @@ class PersonController extends Controller
      */
     public function destroy(Person $person)
     {
-        //
+        try {
+            // Open a transaction on database.
+            DB::beginTransaction();
+
+            // Delete the received person.
+            $response = $person->delete();
+
+            // Confirm the database transaction.
+            DB::commit();
+
+            return $response;
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+    }
+
+    /**
+     * Receive ID from view and call the "PersonController@destroy" method.
+     *
+     * @param integer $id
+     * @return void
+     */
+    public static function delete($id)
+    {
+        try {
+            // Select the person with id is equal to received id on route.
+            $person = Person::find($id);
+
+            // Call the "destroy" method, to delete the selected person.
+            $controller = new PersonController();
+            $controller->destroy($person);
+
+            // Redirects to homepage after delete the person.
+            return redirect('/');
+        } catch (\Throwable $th) {
+            throw $th;
+        }
     }
 }
