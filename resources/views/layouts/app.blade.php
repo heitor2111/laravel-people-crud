bd59c1cad02e891e568fb52106cc2f243c1fef88
Heitor Henrique <heitor2111@gmail.com>
2022-06-08 22:21:17 -0300

    Updates on App Layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,10 +20,32 @@
         integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor">
 </head>
 
-<body>
-    <h2 class="text-center bg-primary p-3">CRUD de Pessoas</h2>
+<body class="vh-100">
+    <div class="h-100 d-flex flex-column">
+        <div class="d-flex px-3 py-2 bg-primary justify-content-between align-items-center">
+            <h3 class="text-white m-0">CRUD de Pessoas</h3>
 
-    @yield('content')
+            <ul class="nav">
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="{{ url('/') }}">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link text-white" href="{{ url('/create') }}">Nova Pessoa</a>
+                </li>
+            </ul>
+        </div>
+
+        <div class="flex-fill overflow-auto p-2">
+            @yield('content')
+        </div>
+
+        @yield('footer')
+    </div>
+
+    <!-- JavaScript Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
