5159b1e1d45dcbb22f82545460c8d5c1247b883f
Heitor Henrique <heitor2111@gmail.com>
2022-06-10 00:15:24 -0300

    Add comments on code

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -42,9 +42,11 @@ class PersonController extends Controller
      */
     public function create()
     {
+        // Get list of all professions sorted by name.
         $professions = Profession::orderByDesc('name')
             ->get();
 
+        // Call the Person/Create view, sending the professions data.
         return view(
             'person.create',
             ['professions' => $professions]
@@ -60,6 +62,7 @@ class PersonController extends Controller
     public function store(Request $request)
     {
         try {
+            // Validate if the request received correct data.
             $request->validate([
                 'first_name' => 'required',
                 'last_name' => 'required',
@@ -73,6 +76,7 @@ class PersonController extends Controller
                 'email' => 'nullable|email',
             ]);
 
+            // Instance a new Person and set some values.
             $person = new Person();
 
             $person->first_name = $request->first_name;
@@ -83,18 +87,23 @@ class PersonController extends Controller
                 $person->birth_date = $request->birth_date;
             }
 
+            // Open a transaction on database.
             DB::beginTransaction();
 
+            // Save Person and refresh model to obtain the Person ID.
             $person->save();
             $person->refresh();
 
+            // Instance a new User and set some values.
             $user = new User();
 
             $user->username = $request->username;
-            $user->password = Hash::make($request->password);
+            $user->password = Hash::make($request->password); // Encrypt the received password.
 
+            // Save the user on database.
             $person->users()->save($user);
 
+            // Validate the contact types and add on array to save.
             $contacts = [];
 
             if ($request->phone_1) {
@@ -118,18 +127,23 @@ class PersonController extends Controller
                 ]));
             }
 
+            // If have contacts on array, save the contacts on database.
             if (sizeof($contacts) > 0) {
                 $person->contacts()->saveMany($contacts);
             }
 
+            // If have a profession, save him on database.
             if ($request->profession) {
                 $person->professions()->attach($request->profession);
             }
 
+            // Confirm the database transaction.
             DB::commit();
 
+            // Redirect to homepage.
             return redirect('/');
         } catch (\Throwable $th) {
+            // If have a error during the transaction, cancel all changes.
             DB::rollBack();
 
             throw $th;
@@ -151,6 +165,7 @@ class PersonController extends Controller
                 ->where('id', $request->id)
                 ->first();
 
+            // If not encounter a user, redirect to homepage.
             if (!$person) {
                 return redirect('/');
             }
@@ -173,6 +188,7 @@ class PersonController extends Controller
      */
     public function edit(Request $request)
     {
+        // Get list of all professions sorted by name.
         $professions = Profession::orderByDesc('name')
             ->get();
 
@@ -182,16 +198,19 @@ class PersonController extends Controller
             ->where('id', $request->id)
             ->first();
 
+        // If not encounter a user, redirect to homepage.
         if (!$person) {
             return redirect('/');
         }
 
+        // If encountered a profession, return the ID of him.
         if (sizeof($person->professions) > 0) {
             foreach ($person->professions as $profession) {
                 $person->professionValue = $profession->id;
             }
         }
 
+        // If encountered contacts, also return.
         if (sizeof($person->contacts) > 0) {
             foreach ($person->contacts as $contact) {
                 if ($contact->description === 'Telefone') {
@@ -206,10 +225,12 @@ class PersonController extends Controller
             }
         }
 
+        // If encountered a user, also return.
         if (sizeof($person->users) > 0) {
             $person->usernameValue = $person->users[0]->username;
         }
 
+        // Call the Person/Edit view, sending the person and professions data.
         return view('person.edit', [
             'professions' => $professions,
             'person' => $person
@@ -227,12 +248,15 @@ class PersonController extends Controller
     public function update(Request $request)
     {
         try {
+            // Select the person with id is equal to received id on route.
             $person = Person::find($request->id);
 
+            // If not encounter a user, redirect to homepage.
             if (!$person) {
                 return redirect('/');
             }
 
+            // Validate if the request received correct data.
             $request->validate([
                 'first_name' => 'required',
                 'last_name' => 'required',
@@ -244,18 +268,22 @@ class PersonController extends Controller
                 'email' => 'nullable|email',
             ]);
 
+            // Set a edited values on model.
             $person->first_name = $request->first_name;
             $person->last_name = $request->last_name;
             $person->birth_date = $request->birth_date;
 
+            // Delete the profession register.
             if (sizeof($person->professions) > 0) {
                 $person->professions()->delete();
             }
 
+            // If received a profession ID, create a new register from him.
             if ($request->profession) {
                 $person->professions()->attach($request->profession);
             }
 
+            // Delete all contacts and if have a edited contacts, register all of them.
             if (sizeof($person->contacts) > 0) {
                 $person->contacts()->delete();
 
@@ -287,19 +315,24 @@ class PersonController extends Controller
                 }
             }
 
+            // If changed the password, encrypt him.
             if ($request->password) {
                 $person->users[0]->password = Hash::make($request->password);
             }
 
+            // Open a transaction on database.
             DB::beginTransaction();
 
+            // Save changes on Person.
             $person->save();
-            $person->refresh();
 
+            // Confirm the database transaction.
             DB::commit();
 
+            // Redirect from Person/View page.
             return redirect("/person/{$request->id}");
         } catch (\Throwable $th) {
+            // If have a error during the transaction, cancel all changes.
             DB::rollBack();
 
             throw $th;
@@ -326,6 +359,7 @@ class PersonController extends Controller
 
             return $response;
         } catch (\Throwable $th) {
+            // If have a error during the transaction, cancel all changes.
             DB::rollBack();
 
             throw $th;
