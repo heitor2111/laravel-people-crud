47f59956193212cfd5fdfbaed60836f962cda0f4
Heitor Henrique <heitor2111@gmail.com>
2022-06-09 23:57:06 -0300

    Create and Edit Person finished

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Contact;
 use App\Models\Person;
+use App\Models\Profession;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class PersonController extends Controller
 {
@@ -37,7 +42,13 @@ class PersonController extends Controller
      */
     public function create()
     {
-        return view('person.create');
+        $professions = Profession::orderByDesc('name')
+            ->get();
+
+        return view(
+            'person.create',
+            ['professions' => $professions]
+        );
     }
 
     /**
@@ -48,7 +59,81 @@ class PersonController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        try {
+            $request->validate([
+                'first_name' => 'required',
+                'last_name' => 'required',
+                'document' => 'required|numeric',
+                'birth_date' => 'nullable|date',
+                'profession' => 'nullable',
+                'username' => 'required',
+                'password' => 'required',
+                'phone_1' => 'nullable|numeric',
+                'phone_2' => 'nullable|numeric',
+                'email' => 'nullable|email',
+            ]);
+
+            $person = new Person();
+
+            $person->first_name = $request->first_name;
+            $person->last_name = $request->last_name;
+            $person->document = $request->document;
+
+            if ($request->birth_date) {
+                $person->birth_date = $request->birth_date;
+            }
+
+            DB::beginTransaction();
+
+            $person->save();
+            $person->refresh();
+
+            $user = new User();
+
+            $user->username = $request->username;
+            $user->password = Hash::make($request->password);
+
+            $person->users()->save($user);
+
+            $contacts = [];
+
+            if ($request->phone_1) {
+                array_push($contacts, new Contact([
+                    'description' => 'Telefone',
+                    'contact' => $request->phone_1
+                ]));
+            }
+
+            if ($request->phone_2) {
+                array_push($contacts, new Contact([
+                    'description' => 'Celular',
+                    'contact' => $request->phone_2
+                ]));
+            }
+
+            if ($request->email) {
+                array_push($contacts, new Contact([
+                    'description' => 'E-mail',
+                    'contact' => $request->email
+                ]));
+            }
+
+            if (sizeof($contacts) > 0) {
+                $person->contacts()->saveMany($contacts);
+            }
+
+            if ($request->profession) {
+                $person->professions()->attach($request->profession);
+            }
+
+            DB::commit();
+
+            return redirect('/');
+        } catch (\Throwable $th) {
+            DB::rollBack();
+
+            throw $th;
+        }
     }
 
     /**
@@ -66,6 +151,10 @@ class PersonController extends Controller
                 ->where('id', $request->id)
                 ->first();
 
+            if (!$person) {
+                return redirect('/');
+            }
+
             // Call the Person/Show view, sending the person data.
             return view(
                 'person.show',
@@ -82,9 +171,50 @@ class PersonController extends Controller
      * @param  \App\Models\Person  $person
      * @return \Illuminate\Http\Response
      */
-    public function edit(Person $person)
+    public function edit(Request $request)
     {
-        return view('person.edit');
+        $professions = Profession::orderByDesc('name')
+            ->get();
+
+        // Select the person with id is equal to received id
+        // on route, along with all your relationships.
+        $person = Person::with(['contacts', 'professions', 'users'])
+            ->where('id', $request->id)
+            ->first();
+
+        if (!$person) {
+            return redirect('/');
+        }
+
+        if (sizeof($person->professions) > 0) {
+            foreach ($person->professions as $profession) {
+                $person->professionValue = $profession->id;
+            }
+        }
+
+        if (sizeof($person->contacts) > 0) {
+            foreach ($person->contacts as $contact) {
+                if ($contact->description === 'Telefone') {
+                    $person->phone1Value = $contact->contact;
+                }
+                elseif ($contact->description === 'Celular') {
+                    $person->phone2Value = $contact->contact;
+                }
+                else {
+                    $person->emailValue = $contact->contact;
+                }
+            }
+        }
+
+        if (sizeof($person->users) > 0) {
+            $person->usernameValue = $person->users[0]->username;
+        }
+
+        return view('person.edit', [
+            'professions' => $professions,
+            'person' => $person
+        ]
+        );
     }
 
     /**
@@ -94,9 +224,86 @@ class PersonController extends Controller
      * @param  \App\Models\Person  $person
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Person $person)
+    public function update(Request $request)
     {
-        //
+        try {
+            $person = Person::find($request->id);
+
+            if (!$person) {
+                return redirect('/');
+            }
+
+            $request->validate([
+                'first_name' => 'required',
+                'last_name' => 'required',
+                'birth_date' => 'nullable|date',
+                'profession' => 'nullable',
+                'password' => 'nullable',
+                'phone_1' => 'nullable|numeric',
+                'phone_2' => 'nullable|numeric',
+                'email' => 'nullable|email',
+            ]);
+
+            $person->first_name = $request->first_name;
+            $person->last_name = $request->last_name;
+            $person->birth_date = $request->birth_date;
+
+            if (sizeof($person->professions) > 0) {
+                $person->professions()->delete();
+            }
+
+            if ($request->profession) {
+                $person->professions()->attach($request->profession);
+            }
+
+            if (sizeof($person->contacts) > 0) {
+                $person->contacts()->delete();
+
+                $contacts = [];
+
+                if ($request->phone_1) {
+                    array_push($contacts, new Contact([
+                        'description' => 'Telefone',
+                        'contact' => $request->phone_1
+                    ]));
+                }
+
+                if ($request->phone_2) {
+                    array_push($contacts, new Contact([
+                        'description' => 'Celular',
+                        'contact' => $request->phone_2
+                    ]));
+                }
+
+                if ($request->email) {
+                    array_push($contacts, new Contact([
+                        'description' => 'E-mail',
+                        'contact' => $request->email
+                    ]));
+                }
+
+                if (sizeof($contacts) > 0) {
+                    $person->contacts()->saveMany($contacts);
+                }
+            }
+
+            if ($request->password) {
+                $person->users[0]->password = Hash::make($request->password);
+            }
+
+            DB::beginTransaction();
+
+            $person->save();
+            $person->refresh();
+
+            DB::commit();
+
+            return redirect("/person/{$request->id}");
+        } catch (\Throwable $th) {
+            DB::rollBack();
+
+            throw $th;
+        }
     }
 
     /**
@@ -119,6 +326,8 @@ class PersonController extends Controller
 
             return $response;
         } catch (\Throwable $th) {
+            DB::rollBack();
+
             throw $th;
         }
     }
@@ -134,6 +343,10 @@ class PersonController extends Controller
         try {
             // Select the person with id is equal to received id on route.
             $person = Person::find($id);
+
+            if (!$person) {
+                return redirect('/');
+            }
 
             // Call the "destroy" method, to delete the selected person.
             $controller = new PersonController();
--- resources/views/person/create.blade.php
+++ resources/views/person/create.blade.php
@@ -1,5 +1,181 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>This is a Create View</h1>
+    <form
+        id="create-form"
+        class="needs-validation"
+        action="{{ route('personStore') }}"
+        method="POST"
+        novalidate
+    >
+        @csrf
+
+        <div class="container py-3">
+            <div class="row">
+                <h4>Dados Pessoais:</h4>
+
+                <div class="col-6">
+                    <label for="first_name" class="form-label">Nome:</label>
+                    <input
+                        id="first_name"
+                        name="first_name"
+                        type="text"
+                        required
+                        placeholder="Insira o nome"
+                        class="form-control @error('first_name') is-invalid @enderror"
+                    >
+
+                    @error('first_name')
+                        <small class="is-invalid pb-3">{{ $message }}</small>
+                    @enderror
+                </div>
+
+                <div class="col-6">
+                    <label for="last_name" class="form-label">Sobrenome:</label>
+                    <input
+                        id="last_name"
+                        name="last_name"
+                        type="text"
+                        required
+                        placeholder="Insira o sobrenome"
+                        class="form-control @error('last_name') is-invalid @enderror"
+                    >
+
+                    @error('last_name')
+                        <small class="is-invalid pb-3">{{ $message }}</small>
+                    @enderror
+                </div>
+
+                <div class="col-sm-12 col-md-4 col-lg">
+                    <label for="document" class="form-label">Documento:</label>
+                    <input
+                        id="document"
+                        name="document"
+                        type="text"
+                        required
+                        maxlength="14"
+                        placeholder="Insira o documento"
+                        class="form-control @error('document') is-invalid @enderror"
+                    >
+
+                    @error('document')
+                        <small class="is-invalid pb-3">{{ $message }}</small>
+                    @enderror
+                </div>
+
+                <div class="col-sm-12 col-md">
+                    <label for="birth_date" class="form-label">Data de Nascimento:</label>
+                    <input
+                        id="birth_date"
+                        name="birth_date"
+                        type="date"
+                        class="form-control"
+                        placeholder="Selecione a data de nascimento"
+                    >
+                </div>
+
+                <div class="col-sm-12 col-md">
+                    <label for="profession" class="form-label">Profissão:</label>
+                    <select
+                        id="profession"
+                        name="profession"
+                        class="form-select"
+                    >
+                        <option value="" disabled selected hidden>Selecione a profissão</option>
+                        @foreach ($professions as $profession)
+                            <option value="{{ $profession->id }}">{{ $profession->name }}</option>
+                        @endforeach
+                    </select>
+                </div>
+            </div>
+
+            <div class="row pt-4">
+                <h4>Dados de Acesso:</h4>
+
+                <div class="col-sm-12 col-md">
+                    <label for="username">Nome de Usuário:</label>
+                    <input
+                        class="form-control @error('username') is-invalid @enderror"
+                        id="username"
+                        name="username"
+                        placeholder="Insira o nome de usuário"
+                        required
+                    >
+
+                    @error('username')
+                        <small class="is-invalid pb-3">{{ $message }}</small>
+                    @enderror
+                </div>
+
+                <div class="col-sm-12 col-md">
+                    <label for="password">Senha:</label>
+                    <input
+                        class="form-control @error('password') is-invalid @enderror"
+                        id="password"
+                        name="password"
+                        type="password"
+                        placeholder="Insira uma senha segura"
+                        required
+                    >
+
+                    @error('password')
+                        <small class="is-invalid pb-3">{{ $message }}</small>
+                    @enderror
+                </div>
+            </div>
+
+            <div class="row pt-4">
+                <h4>Dados de Contato:</h4>
+
+                <div class="col-sm-12 col-md">
+                    <label for="phone_1">Telefone:</label>
+                    <input
+                        class="form-control @error('phone_1') is-invalid @enderror"
+                        id="phone_1"
+                        name="phone_1"
+                        placeholder="Insira o telefone"
+                    >
+
+                    @error('phone_1')
+                        <small class="is-invalid pb-3">{{ $message }}</small>
+                    @enderror
+                </div>
+
+                <div class="col-sm-12 col-md">
+                    <label for="phone_2">Celular:</label>
+                    <input
+                        class="form-control @error('phone_2') is-invalid @enderror"
+                        id="phone_2"
+                        name="phone_2"
+                        placeholder="Insira o celular"
+                    >
+
+                    @error('phone_2')
+                        <small class="is-invalid pb-3">{{ $message }}</small>
+                    @enderror
+                </div>
+
+                <div class="col-sm-12 col-md">
+                    <label for="email">E-mail:</label>
+                    <input
+                        type="email"
+                        class="form-control @error('email') is-invalid @enderror"
+                        id="email"
+                        name="email"
+                        placeholder="Insira o e-mail"
+                    >
+
+                    @error('email')
+                        <small class="is-invalid pb-3">{{ $message }}</small>
+                    @enderror
+                </div>
+            </div>
+        </div>
+    </form>
+@endsection
+
+@section('footer')
+    <div class="d-flex justify-content-end p-2 border-top border-primary border-3">
+        <button type="submit" form="create-form" class="btn btn-primary w-25">Cadastrar</button>
+    </div>
 @endsection
